3168af94640d7c003c3bb24af19c044fa4207621
hansenu <ucabhansen@hotmail.com>
2020-11-21 15:40:35 -0500

    Assignment Submission #2

--- Project2_Functions/JS/main.js
+++ Project2_Functions/JS/main.js
@@ -1,13 +1,13 @@
-function onMouse()   {
-    document.getElementById("any").style.color="red";    
+function onMouse()   {   //Defining the function
+    document.getElementById("any").style.color="red";  //This will change the color to red for variable any  
 }
 
-function outMouse()   {
-    document.getElementById("any").style.color="black";    
+function outMouse()   { //Defining outmouse function
+    document.getElementById("any").style.color="black";  //Changing the color to black on outmouse fx  
 }
 
-function myFunction()   {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("concatenate").innerHTML=sentence;
+function myFunction()   {   //Defining a function
+    var sentence = "I am learning"; //Defining a variable
+    sentence += " a lot from this book!"; //Adding more to the previous variable
+    document.getElementById("concatenate").innerHTML=sentence; //result of the function
 }
