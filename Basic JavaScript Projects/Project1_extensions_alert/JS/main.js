448138570c354d92420aa032a6a90bc7f09f2fe8
hansenu <ucabhansen@hotmail.com>
2020-11-21 13:59:41 -0500

    Assignment Submission #1
    Assignment 1

--- Basic JavaScript Projects/Project1_extensions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_extensions_alert/JS/main.js
@@ -1,2 +1,10 @@
-document.write(34+32)
+function My_First_Function()    {   //Defining a function and naming it
+    var str = "This text is" + " green"; //Defining a variable and giving it a string value
+    var result = str.fontcolor("green");    //Using the fontcolor method to set the str color
+    document.getElementById("Green_Text").innerHTML = result;   //Putting the value of result into HTML element with "Green_Text" id
+}
+
+window.alert("This is a test for assignment #1.") //Pops up everytime html is loaded
+
+document.write("Click that button now!")    //Instructs to click
 
