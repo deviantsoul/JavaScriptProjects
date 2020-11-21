2ad057f5a906d7c7173eb665a0a0ab6f935ed968
hansenu <ucabhansen@hotmail.com>
2020-11-21 17:40:01 -0500

    Assigntment #18

--- Project3_Math/JS/main.js
+++ Project3_Math/JS/main.js
@@ -7,3 +7,13 @@ function subtract_numbers()     {
     var subtraction = 5 - 2;
     document.getElementById("Math2").innerHTML="5-2= " + subtraction;
 }
+
+function multiply_numbers()     {
+    var multiply= 6*8;
+    document.getElementById("Math3").innerHTML="6 times 8 is = " + multiply;
+}
+
+function divide_numbers()       {
+    var divide = 48/6;
+    document.getElementById("Math4").innerHTML="48 divide it by 6 is = " + divide;
+}
