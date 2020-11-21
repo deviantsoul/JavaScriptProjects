1fe4ebe449b75c4b461145e175c891c66593a8d4
hansenu <ucabhansen@hotmail.com>
2020-11-21 17:34:32 -0500

    Assignment #17

--- Project3_Math/JS/main.js
+++ Project3_Math/JS/main.js
@@ -2,3 +2,8 @@ function add_numbers()  {
     var addition = 2+2;
     document.getElementById("Math").innerHTML="2+2=" + addition;
 }
+
+function subtract_numbers()     {
+    var subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML="5-2= " + subtraction;
+}
