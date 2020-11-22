79a46c8b486d99f9489a341b4280d2ca0c595498
hansenu <ucabhansen@hotmail.com>
2020-11-22 13:47:45 -0500

    Challenge 8 (Ternary)
    Ternary options changed to vote.

--- Project6_Ternary_Operators_constructors/JS/main.js
+++ Project6_Ternary_Operators_constructors/JS/main.js
@@ -1,8 +1,8 @@
-function Ride_Function()    {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height <52) ? "You are too short": "You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+function Vote_Function()    {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age <18) ? "I'm sorry, you are not": "Yes you can! You are allowed";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
 document.write(Smaller = (5<3) ? "Left is smaller": "Right is smaller");
