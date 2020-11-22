1792f1ba2b1df7f5b05e91186adc46203cbd6dc6
hansenu <ucabhansen@hotmail.com>
2020-11-22 10:11:05 -0500

    Assignment 28
    Boolean logic

--- Project5_Comparisons_Type_Coercion/JS/main.js
+++ Project5_Comparisons_Type_Coercion/JS/main.js
@@ -11,3 +11,5 @@ document.getElementById("Test4").innerHTML = (5E310);
 function my_Negative_Infinity()  {
     document.getElementById("Test5").innerHTML = (-5E310);
     }
+
+document.write(10>3) AND (40<3);
