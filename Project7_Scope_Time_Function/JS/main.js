9f5ff9934b93ab5d82aa487632ada92ef79583c0
hansenu <ucabhansen@hotmail.com>
2020-11-22 22:21:22 -0500

    Assignment Submission 7
    else, elseif, else
    
    Conditions

--- Project7_Scope_Time_Function/JS/main.js
+++ Project7_Scope_Time_Function/JS/main.js
@@ -1,67 +1,67 @@
-var X = 20
-function Add_numbers_1()    {
-    document.write(20 + X + "<br>")
+var X = 20      //global variables
+function Add_numbers_1()    {       //defining a function
+    document.write(20 + X + "<br>")     //Print results
 }
-function Add_numbers_2()    {
-    document.write( X + 10);
+function Add_numbers_2()    {       //defining another function
+    document.write( X + 10);        //printing results from global variables
 }
-Add_numbers_1();
-Add_numbers_2();
+Add_numbers_1();        //additions
+Add_numbers_2();        //additions
 
-function Add_numbers_3()    {
-    var Y = 10;
-    document.write(20 + Y + Y + Y);
+function Add_numbers_3()    {       //defining a function
+    var Y = 10;     //setting up local variables
+    document.write(20 + Y + Y + Y);     //result
 }
-function Add_numbers_4()    {
-    document.write( Y + Y + 20);
+function Add_numbers_4()    {       //defining function
+    document.write( Y + Y + 20);    //result but without local variables
 }
-Add_numbers_3();
-Add_numbers_4();
+Add_numbers_3();        //add 2 functions
+Add_numbers_4();        //add 2 functions
 
-function Add_numbers_5()    {
-    var Z = 5;
-    console.log( 10 + Z);
+function Add_numbers_5()    {       //defining function
+    var Z = 5;      //setting up local variable
+    console.log( 10 + Z);       //result in log only
 }
 
-function Add_numbers_6()    {
-    console.log( 100 + Z)
+function Add_numbers_6()    {       //definig function
+    console.log( 100 + Z)           //result in log only should be error
 }
-Add_numbers_5();
-Add_numbers_6();
+Add_numbers_5();        //adding both functions
+Add_numbers_6();        //adding both functions
 
-function get_Date() {
-    if (new Date().getHours()>18)   {
-        document.getElementById("Greeting").innerHTML = "How was your day sir?";
+function get_Date() {       //getting the date function or time to be precise
+    if (new Date().getHours()>18)   {       //if conditions met
+        document.getElementById("Greeting").innerHTML = "How was your day sir?";  //result of if condition
     }
 }
 
-function get_Time()     {
-    document.getElementById("CurrentTime").innerHTML = Date();
+function get_Time()     {       //just my own thing to try add the current time, date
+    document.getElementById("CurrentTime").innerHTML = Date();      //result
 }
 
-function Game_Function()     {
-    var letter = document.getElementById("Game").value;
-    var text;    
-    if ( letter === "Cyberpunk")    {
-        Text = "You are awesome!";
+function Game_Function()     {          //function for my game function
+    var letter = document.getElementById("Game").value;     //setting a value from id
+    var text;   //setting the variable type 
+    if ( letter === "Cyberpunk")    {       //variable should equal the condition
+        Text = "You are awesome!";          //if met, result
+    }
+    else    {          
+        Text = "You suck!";         //if condition not equal, result
+    }
+    document.getElementById("Game you playing right now").innerHTML = Text;     //display result
+}
+
+function Time_Function()    {           //get time
+    var Time = new Date().getHours();       // setting a variable
+    var Reply;              //setting another variable but the end result
+    if (Time <12 == Time > 0)   {           //condition numba 1
+        Reply = "It is morning time!";      //result if met
+    }
+    else if (Time > 12 == Time <18) {       //condition numba 2
+        Reply = "It is the afternoon.";     //result if met
     }
     else    {
-        Text = "You suck!";
+        Reply = "Time to sleep.";       // or else this
     }
-    document.getElementById("Game you playing right now").innerHTML = Text;
-}
-
-function Time_Function()    {
-    var Time = new Date().getHours();
-    var Reply;
-    if (Time <12 == Time > 0)   {
-        Reply = "It is morning time!";
-    }
-    else if (Time > 12 == Time <18) {
-        Reply = "It is the afternoon.";
-    }
-    else    {
-        Reply = "Time to sleep.";
-    }
-    document.getElementById("Time of the day").innerHTML = Reply;
+    document.getElementById("Time of the day").innerHTML = Reply; //result of all conditions
 }
