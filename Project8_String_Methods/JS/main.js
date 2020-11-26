144a1ccf87396c5cc07c8b9e43bcdacf2bce60ea
hansenu <ucabhansen@hotmail.com>
2020-11-26 14:00:57 -0500

    Challenge 10
    search method
    toUppercase

--- Project8_String_Methods/JS/main.js
+++ Project8_String_Methods/JS/main.js
@@ -11,3 +11,15 @@ function slice_method()     {
     var Section = Sentence.slice(27,47)
     document.getElementById("Slice").innerHTML = Section;
 }
+
+function my_Upper()     {
+    var str = "Challenge 10"
+    var up = str.toUpperCase();
+    document.getElementById("Upper").innerHTML = up;
+}
+
+function on_search()        {
+    var str = "Dark today right? Obviously, it's dark and gloomy everywhere!"
+    var me = str.search("dark");
+    document.getElementById("challenge").innerHTML = me;
+}
