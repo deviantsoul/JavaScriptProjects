e93020436a11c987e26f61767381ff224c2540e3
hansenu <ucabhansen@hotmail.com>
2020-11-26 13:25:27 -0500

    Assignment 44
    concat()

---
+++ Project8_String_Methods/JS/main.js
@@ -0,0 +1,7 @@
+function dumb()     {
+    var part_1 = "Dumb, "
+    var part_2 = "Dumber, "
+    var part_3 = "Dumbest."
+    var whole_dumb = part_1.concat(part_2, part_3);
+    document.getElementById("connect").innerHTML=whole_dumb;
+}
