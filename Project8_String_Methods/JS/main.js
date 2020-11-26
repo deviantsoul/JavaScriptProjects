97af97e064cc5f2b9e904382972bc3aed67bd30b
hansenu <ucabhansen@hotmail.com>
2020-11-26 13:43:27 -0500

    Assignment 45
    slice()

--- Project8_String_Methods/JS/main.js
+++ Project8_String_Methods/JS/main.js
@@ -5,3 +5,9 @@ function dumb()     {
     var whole_dumb = part_1.concat(part_2, part_3);
     document.getElementById("connect").innerHTML=whole_dumb;
 }
+
+function slice_method()     {
+    var Sentence = "Muhammad Ali keeps saying, \"He's the greatest.\" But he never believed it himself.";
+    var Section = Sentence.slice(27,47)
+    document.getElementById("Slice").innerHTML = Section;
+}
